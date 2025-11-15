7f352ebacf89a63a5eb97844ae84abf12f67cccf
Hoang Anh <hanhsociu555@gmail.com>
2025-11-16 01:05:26 +0700

    Hoàn thành giao diên admin và shop

---
+++ database/migrations/2025_11_15_175943_create_drinks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('drinks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('menu_category_id'); // liên kết danh mục
+            $table->string('name');                         // tên đồ uống
+            $table->decimal('price', 10, 2);                // giá
+            $table->string('flavor')->nullable();           // vị
+            $table->json('images')->nullable();             // nhiều ảnh
+            $table->string('type')->nullable();             // loại: "có cồn, không cồn; có ga, không có ga"
+            $table->text('description')->nullable();        // mô tả
+            $table->timestamps();
+
+            // Khóa ngoại liên kết MenuCategory
+            $table->foreign('menu_category_id')
+                ->references('id')
+                ->on('menu_categories')
+                ->onDelete('restrict'); // không xóa danh mục nếu còn đồ uống
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('drinks');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Controller Routes
+
+use App\Http\Controllers\admin\DashboardController;
+
+// Shop Routes
+use App\Http\Controllers\shop\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// ==================== CLIENT SITE ====================
+Route::prefix('/')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    // Route::get('/about', [AboutController::class, 'index'])->name('about');
+    // Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+    // Route::get('/booking', [BookingController::class, 'index'])->name('booking');
+    // Route::get('/menu', [MenuController::class, 'index'])->name('menu');
+    // Route::get('/service', [ServiceController::class, 'index'])->name('service');
+    // Route::get('/team', [TeamController::class, 'index'])->name('team');
+    // Route::get('/testimonial', [TestimonialController::class, 'index'])->name('testimonial');
 });
+
+// ==================== ADMIN SITE ====================
+Route::prefix('admin')->name('admin.')->group(
+    function () {
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    }
+);
